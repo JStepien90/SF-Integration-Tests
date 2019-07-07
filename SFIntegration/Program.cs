e3b7bd07fc77ddc2e4505e40488dafe0fffa9c2b
Kuba&Paula <Kuba&Paula@DESKTOP-AHKJHVG>
2019-07-07 19:55:27 +0200

    Add project files.

---
+++ SFIntegration/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ServiceModel;
+using SF;
+
+namespace SFIntegration
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            EndpointAddress endpointAddress = new EndpointAddress("");
+
+            SoapClient soapClient = new SoapClient();
+
+            var loginResponse = await soapClient.loginAsync(new LoginScopeHeader(), 
+                                                            "jakubs.stepien.90@gmail.com", 
+                                                            "!HalaMadrid@20197uAFwMrL7g23pNrQ6sqrNzMd");
+
+
+        }
+    }
+}
